469d95c1ab8a2a037f7d7567e00b9ae0abc95476
Katerina Sioumpoura <122751700+katsioump@users.noreply.github.com>
2023-07-04 14:58:09 +0100

    Update 5.1_TPCs_VeDBA.R

--- analyses/5.1_TPCs_VeDBA.R
+++ analyses/5.1_TPCs_VeDBA.R
@@ -13,6 +13,11 @@ tl_all_param <- data.frame(code=character(),
                  breadth=integer(),
                  stringsAsFactors=FALSE) 
 
+source("C:\\Users\\Katerina\\Documents\\Master thesis\\R scripts\\calc_params1.R")
+source("C:\\Users\\Katerina\\Documents\\Master thesis\\R scripts\\get_tmax.R")
+source("C:\\Users\\Katerina\\Documents\\Master thesis\\R scripts\\get_tmin.R")
+
+
 for (i in c(5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25)){
   s <- str_pad(i, 2, pad = "0")
   path <- paste0("C:\\Users\\Katerina\\Desktop\\mesocosms\\Tb_first\\TLCRP0", s, ".csv")
@@ -20,15 +25,18 @@ for (i in c(5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25)){
   tl <- subset(tl, select = -c(1))
   tl$code <- paste0("TLCRP0", s)
   
+  active <- which(tl$Move == "mobile")
+  tl <- tl[active,]
+  
   #### Data format
-  #### Create bins for Tb and select the rows with the 10 maximum Tb values ###
+  #### Create bins for Tb and select the rows with the 5 maximum Tb values ###
   tl_c <- subset(tl, select = c("code", "Tb", "VeDBA"))
   tl_c <- tl_c[, c("code", "Tb", "VeDBA")]
   colnames(tl_c) <- c("id", "temp", "rate")
   
   temp <- tl_c$temp
   hist(temp)
-  bins <- ceiling(max(temp) - min(temp))
+  bins <- 5*ceiling(max(temp) - min(temp))
   bins
   
   tl_c <- tl_c %>% mutate(bins = cut(temp, breaks=bins))
@@ -43,7 +51,7 @@ for (i in c(5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25)){
   
   #############################################################################
   
-  ##### 8 models: 4 from Angiletta paper and 4 that look nice from graphs #####
+  ##### 7 models: 4 from Angiletta paper and 4 that look nice from graphs #####
   d_fits <- nest(tl_c, data = c(temp, rate)) %>%
     mutate(gaussian = map(data, ~nls_multstart(rate~gaussian_1987(temp = temp, rmax, topt, a),
                                                data = .x,
@@ -123,7 +131,6 @@ for (i in c(5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25)){
   # stack models
   d_stack <- select(d_fits, -data) %>%
     pivot_longer(., names_to = 'model_name', values_to = 'fit', gaussian:weibull)
-  
   # get parameters using tidy
   params <- d_stack %>%
     mutate(., est = map(fit, tidy)) %>%
@@ -158,7 +165,7 @@ for (i in c(5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25)){
           strip.text = element_text(hjust = 0),
           strip.background = element_blank()) +
     labs(x = 'Body temperature (ºC)',
-         y = 'VeDBA',
+         y = 'VeDBA (g)',
          title = paste0('Fits of selected models for TLCRP0', s)) +
     geom_text(data = data, aes(label=AIC), x = 50/100*(max(tl$Tb)), y = 98/100*(max(tl$VeDBA)),
                size = 5) +
@@ -166,7 +173,7 @@ for (i in c(5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25)){
     geom_hline(aes(yintercept = 0), linetype = 2)
   
   
-  ggsave(paste0("C:/Users/Katerina/Documents/Master thesis/some graphs/TPC models/TLCRP0", s, ".png"), width = 20, height = 10.4)
+  ggsave(paste0("C:/Users/Katerina/Documents/Master thesis/some graphs/TPC models_VeDBA/TLCRP0", s, ".png"), width = 20, height = 10.4)
   
   ## Model selection
   ## In this instance, we will use AICc score to compare between models. For a model selection approach, the model with the lowest AICc score is chosen as the model that best supports the data. In this instance, it is the Sharpe-Schoolfield model.
@@ -175,14 +182,26 @@ for (i in c(5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25)){
   best_model = filter(d_ic, AICc == min(AICc)) %>% pull(model_name)
   best_model
   
+  
   # calculate estimated parameters
   params <- d_stack %>%
-    mutate(., params = map(fit, calc_params)) %>%
+    mutate(., params = map(fit, calc_params1)) %>%
     select(-fit) %>%
     unnest(params)
   
-  tl_param <- data.frame(code = params$id, model = params$model_name, AIC = d_ic$AIC, topt = params$topt, rmax = params$rmax, breadth = params$breadth)
+  tmax <- d_stack %>%
+    mutate(., tmax = map(fit, get_tmax)) %>%
+    select(-fit) %>%
+    unnest(tmax)
+  
+  tmin <- d_stack %>%
+    mutate(., tmin = map(fit, get_tmin)) %>%
+    select(-fit) %>%
+    unnest(tmin)
+  
+  tl_param <- data.frame(code = params$id, model = params$model_name, AIC = d_ic$AIC, topt = params$topt, rmax = params$rmax, breadth = params$breadth, tfmin = tmin$tmin, tfmax = tmax$tmax)
   tl_all_param <- rbind(tl_all_param, tl_param)
 }
 
-write.csv(tl_all_param, file = "C:\\Users\\Katerina\\Documents\\Master thesis\\results\\TPC_params_VeDBA.csv")
+write.csv(tl_all_param, file = "C:\\Users\\Katerina\\Documents\\Master thesis\\results\\TPC_params_new.csv")
+
